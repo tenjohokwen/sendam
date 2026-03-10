e756b37b89d4e7df046cf65eeaf1411952804903
mokwen <mokwen@groupon.com>
2026-03-10 14:33:35 +0100

    feat(01-01): ClientEntity, ClientApiKeyEntity, and repositories
    - Add ClientEntity extending AbstractAuditingEntity, mapped to main.client_account
    - Add ClientRepository (JpaRepository<ClientEntity, Long>)
    - Add ClientApiKeyEntity mapped to main.client_api_key with keyPrefix, keyHash, label, clientId
    - Add ClientApiKeyRepository with findByKeyPrefix and findAllByClientId queries
    - Both entities default status to EntityStatus.ACTIVE (override parent INACTIVE default)

---
+++ src/main/java/com/softropic/sendam/client/repo/ClientEntity.java
@@ -0,0 +1,34 @@
+package com.softropic.sendam.client.repo;
+
+import com.softropic.sendam.common.persistence.AbstractAuditingEntity;
+import com.softropic.sendam.common.persistence.EntityStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "client_account", schema = "main")
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientEntity extends AbstractAuditingEntity {
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Builder.Default
+    protected EntityStatus status = EntityStatus.ACTIVE;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/softropic/sendam/client/repo/ClientRepository.java
@@ -0,0 +1,6 @@
+package com.softropic.sendam.client.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
+}
