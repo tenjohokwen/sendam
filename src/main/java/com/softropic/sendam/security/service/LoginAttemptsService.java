b5eef4e7b18139677c01562655334c340cd072aa
mokwen <mokwen@groupon.com>
2026-03-14 08:06:36 +0100

    improves LoginAttemptsService

--- src/main/java/com/softropic/sendam/security/service/LoginAttemptsService.java
+++ src/main/java/com/softropic/sendam/security/service/LoginAttemptsService.java
@@ -8,8 +8,9 @@ import com.google.common.base.Ticker;
 
 import com.softropic.sendam.security.common.util.RequestMetadata;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +22,11 @@ import java.util.concurrent.TimeUnit;
  * Records failed logins and uses this information to determine if a login can be allowed or not.
  * On a general note, successful login undoes the failed ones. There are however things to take into consideration:
  *  <ul>
-     <li>It should be possible to use a blocked client to log a different user. So use a combination of clientId and username</li>
-     <li>The client id (browser cookie or apikey) could be faked or stolen, leading to unique ids per request. So also take Ips into account.</li>
-     <li>Several people could share an ip address. So avoid blocking just by ip. Use ip and loginId</li>
-     <li>Ip addresses can be faked. So keep a higher count of failed logins for a given user to delay this</li>
-    </ul>
+ <li>It should be possible to use a blocked client to log a different user. So use a combination of clientId and username</li>
+ <li>The client id (browser cookie or apikey) could be faked or stolen, leading to unique ids per request. So also take Ips into account.</li>
+ <li>Several people could share an ip address. So avoid blocking just by ip. Use ip and loginId</li>
+ <li>Ip addresses can be faked. So keep a higher count of failed logins for a given user to delay this</li>
+ </ul>
  *
  * Levels of locking
  * <ol>
@@ -35,7 +36,6 @@ import java.util.concurrent.TimeUnit;
  * </ol>
  */
 //TODO document all changes that need to be made once for the application to be multi-node ready. This cache cannot be used for multi node apps
-@Slf4j
 @Service("loginAttemptService")
 public class LoginAttemptsService implements LoginDecisionManager<RequestMetadata>,
                                              LoginAttemptConsumer<RequestMetadata> {
@@ -45,31 +45,38 @@ public class LoginAttemptsService implements LoginDecisionManager<RequestMetadat
      */
     private static final int MAX_FAILED_CLIENT_ATTEMPTS = 3;
 
+    private static final Logger log = LoggerFactory.getLogger(LoginAttemptsService.class);
+
+    private static final String LOG_TAG = "LOGIN ATTEMPT NOTICE: ";
 
     /*
-    The ip address is blocked when login failure occurs {@code MAX_FAILED_CLIENT_ATTEMPTS } times within a time frame
+    The ip address is blocked when login failure occurs {@code MAX_FAILED_CLIENT_ATTEMPTS} times within a time frame
      */
     private static final int MAX_FAILED_IP_ATTEMPTS = MAX_FAILED_CLIENT_ATTEMPTS;
 
-
     /*
-     A rogue client will be able to make {@code MAX_FAILED_CLIENT_ATTEMPTS + 2 } attempts when it fakes ip addresses and clientIds
+     A rogue client will be able to make {@code MAX_FAILED_CLIENT_ATTEMPTS + 2} attempts when it fakes ip addresses and clientIds
      */
     private static final int MAX_FAILED_USER_ATTEMPTS = MAX_FAILED_CLIENT_ATTEMPTS + 2;
+
+    /**
+     * Separator used to build composite cache keys. Must not appear unescaped in any field value.
+     */
+    private static final String KEY_SEPARATOR = "|";
+    private static final String KEY_SEPARATOR_ESCAPED = "%7C";
 
     private final LoadingCache<String, Integer> blacklistedClients;
 
     //only the userName (loginId) is used as key here
     private final LoadingCache<String, Integer> attemptsByUserCache;
 
-    //ip_userName key
+    //ip|userName key
     private final LoadingCache<String, Integer> attemptsByIpUserCache;
 
-    //clientId_userName key
+    //clientId|userName key
     private final LoadingCache<String, Integer> attemptsByClientUserCache;
 
     private final ClientIdAccessDecisionManager clientIdAccessDecisionVoter;
-
 
 
     @Autowired
@@ -77,24 +84,24 @@ public class LoginAttemptsService implements LoginDecisionManager<RequestMetadat
         this(clientIdAccessDecisionMgr, Ticker.systemTicker());
     }
 
-    // New constructor for testing
+    // Constructor for testing with a controllable ticker
     public LoginAttemptsService(final ClientIdAccessDecisionManager clientIdAccessDecisionVoter, final Ticker ticker) {
-        attemptsByUserCache = buildCache(ticker);
-        attemptsByIpUserCache = buildCache(ticker);
-        attemptsByClientUserCache = buildCache(ticker);
-        blacklistedClients = buildCache(ticker); // Assuming blacklistedClients also needs controlled expiry
+        attemptsByUserCache      = buildCache(ticker, 4,  TimeUnit.HOURS);
+        attemptsByIpUserCache    = buildCache(ticker, 4,  TimeUnit.HOURS);
+        attemptsByClientUserCache = buildCache(ticker, 4, TimeUnit.HOURS);
+        blacklistedClients       = buildCache(ticker, 30, TimeUnit.DAYS);
         this.clientIdAccessDecisionVoter = clientIdAccessDecisionVoter;
     }
 
     @Override
     public void loginSucceeded(final RequestMetadata metadata) {
-        log.info("################# Login succeeded. Clear failed login attempts");
+        log.info("Login succeeded. Clearing failed login attempts.");
         deRecordAttempts(metadata);
     }
 
     @Override
     public void loginFailed(final RequestMetadata metadata) {
-        log.info("Login failed. about to Record failed login attempts");
+        log.info("Login failed. Recording failed login attempts.");
         //username will be blank if userName validation fails
         if(StringUtils.isNotBlank(metadata.getUserName())) {
             recordAttempts(attemptsByUserCache, metadata.getUserName());
@@ -105,44 +112,77 @@ public class LoginAttemptsService implements LoginDecisionManager<RequestMetadat
 
     @Override
     public boolean isAllowed(final RequestMetadata metadata) {
-        log.info("################# Verify if login is allowed based on previous attempts");
+        log.info("Verifying if login is allowed based on previous attempts.");
+        if(StringUtils.isBlank(metadata.getUserName())) {
+            // Nothing is recorded for blank usernames (see loginFailed).
+            // Upstream validation should reject blank usernames before this point.
+            return true;
+        }
         try {
-            return  attemptsByClientUserCache.get(getClientIdUserKey(metadata)) < MAX_FAILED_CLIENT_ATTEMPTS
+            return  isBelowClientAttemptLimit(metadata)
                     &&
                     isIpAndUserAllowed(metadata)
                     &&
-                    attemptsByUserCache.get(metadata.getUserName()) < MAX_FAILED_USER_ATTEMPTS
+                    isBelowUserAttemptLimit(metadata)
                     &&
-                    clientIdAccessDecisionVoter.isClientIdAllowed() //Explicitly invoke this since call does not go down to controller lever. AccessDecisionVoters are called by interceptors. (interceptors are called before controllers)
+                    isClientIdAllowed() //Explicitly invoke this since call does not go down to controller level. AccessDecisionVoters are called by interceptors. (interceptors are called before controllers)
                     &&
-                    clientNotBlacklisted(metadata.getClientIdentifier()) ;
+                    clientNotBlacklisted(metadata.getClientIdentifier());
         } catch (ExecutionException e) {
+            log.error("{} Unexpected cache error during login decision, failing open.", LOG_TAG, e);
             return true;
         }
     }
 
+    private boolean isClientIdAllowed() {
+        final boolean clientIdAllowed = clientIdAccessDecisionVoter.isClientIdAllowed();
+        if(!clientIdAllowed) {
+            log.error("{} Client id is already blacklisted." , LOG_TAG);
+        }
+        return clientIdAllowed;
+    }
+
+    private boolean isBelowUserAttemptLimit(RequestMetadata metadata) throws ExecutionException {
+        final String userName = metadata.getUserName();
+        final boolean isBelowUserAttemptLimit = attemptsByUserCache.get(userName) < MAX_FAILED_USER_ATTEMPTS;
+        if(!isBelowUserAttemptLimit) {
+            log.error("{} The current user has exceeded permitted login attempts for current window. User: {}", LOG_TAG,
+                      userName);
+        }
+        return isBelowUserAttemptLimit;
+    }
+
+    private boolean isBelowClientAttemptLimit(RequestMetadata metadata) throws ExecutionException {
+        final boolean isBelowMaxClientAttempts = attemptsByClientUserCache.get(getClientIdUserKey(metadata)) < MAX_FAILED_CLIENT_ATTEMPTS;
+        if(!isBelowMaxClientAttempts) {
+            log.error("{} Client login attempts exceed amount permitted within window", LOG_TAG );
+        }
+        return isBelowMaxClientAttempts;
+    }
+
     private boolean clientNotBlacklisted(final String clientId) throws ExecutionException {
         if(StringUtils.isNotBlank(clientId)) {
-            return blacklistedClients.get(clientId) == 0;
+            final boolean isNotBlacklisted = blacklistedClients.get(clientId) == 0;
+            if(!isNotBlacklisted) { //is blacklisted
+                log.error("{} Client is already blacklisted." , LOG_TAG);
+            }
+            return isNotBlacklisted;
         }
-        //bcookie should actually not yet exist
-        //If bcookie exists at login then it is a fraud
-        //use clientjs to create cookie called fcookie (fingerprint)
-        //When user authenticates, create the bcookie using the fcookie
-        //The lifespan of the fcookie should be same as jwt
-        //renew bcookie when renewing jwt
         return true;
     }
 
     /**
      * Uses client identifier to blacklist client.
+     * Note: blacklisting IS enforced in isAllowed() via clientNotBlacklisted().
+     * What's missing: persistence. Blacklisted clients are only held in-memory and expire
+     * after the cache window. For permanent blacklisting, persist to DB here.
      * @param metadata provides the client data.
      */
     @Override
-    public void blacklistClient(final RequestMetadata metadata) { //TODO not yet making use of this to prevent users. Only recording.
+    public void blacklistClient(final RequestMetadata metadata) {
         //TODO could create a table in db and add a service here to populate blacklisted clients and the reasons
         //Blacklisted clients will not have access to the system anymore regardless of the user they use
-        log.warn("Fraud detection from client with the following metadata {}", metadata);
+        log.warn("Blacklisting client with metadata {}", metadata);
         //The user is probably not known so just the client is blacklisted.
         //The getClientIdentifier here may not exist e.g for a browser (or else many users will share the same key)
         if(StringUtils.isNotBlank(metadata.getClientIdentifier())) {
@@ -156,34 +196,38 @@ public class LoginAttemptsService implements LoginDecisionManager<RequestMetadat
 
     @Override
     public void unblockClient(final RequestMetadata metadata) {
-        log.info("################# Unblock client");
+        log.info("Unblocking client.");
         deRecordAttempts(metadata);
     }
 
     private String getClientIdUserKey(final RequestMetadata metadata) {
-        return metadata.getClientIdentifier() + "_" + metadata.getUserName();
+        return escapeField(StringUtils.defaultString(metadata.getClientIdentifier())) + KEY_SEPARATOR + escapeField(metadata.getUserName());
     }
 
     private String getIpUserKey(final RequestMetadata metadata) {
-        String ipAddress = StringUtils.isBlank(metadata.getIpAddress()) ? "" : metadata.getIpAddress();
-        return ipAddress + "_" + metadata.getUserName();
+        String ipAddress = StringUtils.defaultString(metadata.getIpAddress());
+        return escapeField(ipAddress) + KEY_SEPARATOR + escapeField(metadata.getUserName());
     }
 
+    /**
+     * Escapes the key separator character in a field value to prevent composite key collisions.
+     */
+    private String escapeField(final String value) {
+        return value.replace(KEY_SEPARATOR, KEY_SEPARATOR_ESCAPED);
+    }
 
+    /**
+     * Atomically increments the attempt count for the given identifier.
+     */
     private void recordAttempts(final LoadingCache<String, Integer> cache, final String identifier) {
-        int attempts;
-        try {
-            attempts = cache.get(identifier);
-        } catch (ExecutionException e) {
-            attempts = 0;
-        }
-        attempts++;
-        cache.put(identifier, attempts);
+        cache.asMap().merge(identifier, 1, Integer::sum);
     }
 
     /**
      * Removes all previously failed attempts of the client-user combination.
-     * Since this count augmented the other caches, it has to decrement them on removal.
+     * Since this count also incremented the other caches, it decrements them on removal.
+     * The user-level cache is always fully cleared on success, covering the case where the
+     * user previously failed from a different client or IP than the one used for this success.
      * @param metadata holds the client info needed for decrementing the cache counts.
      */
     private void deRecordAttempts(final RequestMetadata metadata) {
@@ -193,7 +237,7 @@ public class LoginAttemptsService implements LoginDecisionManager<RequestMetadat
             attempts = attemptsByClientUserCache.get(clientIdUserKey);
             attemptsByClientUserCache.invalidate(clientIdUserKey);
             if(attempts > 0) {
-                //The failed attempts were equally recorded in the other 2 caches. Reduce them
+                //The failed attempts were equally recorded in the other 2 caches. Reduce them.
                 // NB This is not accurate but the best way to go about this
                 // NB The user may have had failed logins using the same client but different ips
                 // NB The net effect is that the reduction occurs. In most cases the user will use the same ip though
@@ -204,45 +248,47 @@ public class LoginAttemptsService implements LoginDecisionManager<RequestMetadat
         } catch (ExecutionException e) {
             log.debug("'{}' not found in 'attemptsByClientUserCache'.", clientIdUserKey, e);
         }
+        // Always clear the user-level lock regardless of which client was used during failures.
+        // This covers the case where the successful login comes from a different client than the failed attempts.
+        attemptsByUserCache.invalidate(metadata.getUserName());
     }
 
+    /**
+     * Atomically decrements the attempt count for the given identifier.
+     * Removes the entry if the count reaches zero or below.
+     */
     private void countDownAttempts(final LoadingCache<String, Integer> cache, final String identifier, final int count) {
-        int attempts;
-        try {
-            attempts = cache.get(identifier);
-            attempts = attempts - count;
-            if(attempts > 0) {
-                cache.put(identifier, attempts);
-            } else {
-                cache.invalidate(identifier);
-            }
-        } catch (ExecutionException e) {
-            log.warn("Error occurred upon execution of countdown attempts", e);
-        }
+        cache.asMap().computeIfPresent(identifier, (k, v) -> {
+            int result = v - count;
+            return result > 0 ? result : null; // returning null removes the entry
+        });
     }
-
 
     private boolean isIpAndUserAllowed(final RequestMetadata metadata) {
         //TODO also verify that ip is in whitelist
         try {
-            return attemptsByIpUserCache.get(getIpUserKey(metadata)) < MAX_FAILED_IP_ATTEMPTS;
+            final boolean isAttemptsByIpBelowLimit = attemptsByIpUserCache.get(getIpUserKey(metadata)) < MAX_FAILED_IP_ATTEMPTS;
+            if(!isAttemptsByIpBelowLimit) {
+                log.error(LOG_TAG + "Current IP address has exceeded login attempt limits for given window");
+            }
+            return isAttemptsByIpBelowLimit;
         } catch (ExecutionException e) {
+            log.error("{} Unexpected cache error during IP check, failing open.", LOG_TAG, e);
             return true;
         }
     }
 
-    // Modified buildCache to accept and use a Ticker
-    private LoadingCache<String, Integer> buildCache(Ticker ticker) {
+    private LoadingCache<String, Integer> buildCache(Ticker ticker, long duration, TimeUnit unit) {
         return CacheBuilder.newBuilder()
-                           .ticker(ticker) // Use the provided ticker
-                           .expireAfterWrite(4, TimeUnit.HOURS)
+                           .ticker(ticker)
+                           .expireAfterWrite(duration, unit)
                            .build(new CacheLoader<>() {
-                    @SuppressWarnings("NullableProblems")
-                    @Override
-                    public Integer load(final String key) {
-                        return 0;
-                    }
-                });
+                               @SuppressWarnings("NullableProblems")
+                               @Override
+                               public Integer load(final String key) {
+                                   return 0;
+                               }
+                           });
     }
 
 }
--- src/test/java/com/softropic/sendam/security/service/LoginAttemptsServiceTest.java
+++ src/test/java/com/softropic/sendam/security/service/LoginAttemptsServiceTest.java
@@ -20,21 +20,19 @@ class LoginAttemptsServiceTest {
     private LoginAttemptsService loginAttemptsService;
     private ClientIdAccessDecisionManager defaultDecisionVoter;
 
-    // Default values for TestRequestMetadataProvider state
-    private static final String DEFAULT_TEST_USERNAME = "defaultUser@example.com";
+    private static final String DEFAULT_TEST_USERNAME  = "defaultUser@example.com";
     private static final String DEFAULT_TEST_CLIENT_ID = "defaultClient";
     private static final String DEFAULT_TEST_IP_ADDRESS = "127.0.0.1";
 
-    // Thresholds from LoginAttemptsService (consider making them package-private or test constants if possible)
+    // Mirror thresholds from LoginAttemptsService
     private static final int MAX_FAILED_CLIENT_ATTEMPTS = 3;
-    private static final int MAX_FAILED_IP_ATTEMPTS =  MAX_FAILED_CLIENT_ATTEMPTS; //MAX_FAILED_CLIENT_ATTEMPTS * 2;
-    private static final int MAX_FAILED_USER_ATTEMPTS = MAX_FAILED_CLIENT_ATTEMPTS + 2;
+    private static final int MAX_FAILED_IP_ATTEMPTS     = MAX_FAILED_CLIENT_ATTEMPTS;
+    private static final int MAX_FAILED_USER_ATTEMPTS   = MAX_FAILED_CLIENT_ATTEMPTS + 2;
 
 
     @BeforeEach
     void setUp() {
         defaultDecisionVoter = new ClientIdAccessDecisionManager(List.of());
-        // Default service uses system ticker. Tests needing FakeTicker will create their own instance.
         loginAttemptsService = new LoginAttemptsService(defaultDecisionVoter, Ticker.systemTicker());
         resetTestRequestMetadataProvider();
     }
@@ -97,30 +95,64 @@ class LoginAttemptsServiceTest {
         loginAttemptsService.loginSucceeded(TestRequestMetadataProvider.getClientInfo());
         assertLoginAllowed(true, "Allowed after successful login, attempts should be cleared");
 
-        simulateFailedLogins(MAX_FAILED_CLIENT_ATTEMPTS -1);
+        simulateFailedLogins(MAX_FAILED_CLIENT_ATTEMPTS - 1);
         assertLoginAllowed(true, "Allowed after new attempts less than max (post success)");
 
         simulateFailedLogins(1);
         assertLoginAllowed(false, "Blocked after new attempts reach max (post success)");
     }
 
-    // Helper for tests that modify client ID or IP randomly
+    /**
+     * Bug fix verification: successful login from a different client than the one used during failures
+     * must still clear the user-level lock.
+     */
+    @Test
+    void givenFailedAttemptsOnOneClient_whenLoginSucceedsOnDifferentClient_thenUserLevelLockCleared() {
+        // Fail MAX_FAILED_USER_ATTEMPTS times from unique clients/IPs to trigger user-level block
+        for (int i = 0; i < MAX_FAILED_USER_ATTEMPTS; i++) {
+            TestRequestMetadataProvider.setBrowserCookie(RandomStringUtils.randomAlphabetic(10));
+            TestRequestMetadataProvider.setIpAddress(randomIp());
+            loginAttemptsService.loginFailed(TestRequestMetadataProvider.getClientInfo());
+        }
+
+        // Use a fresh client + IP to confirm the user is blocked at user level
+        TestRequestMetadataProvider.setBrowserCookie("freshClient");
+        TestRequestMetadataProvider.setIpAddress("10.99.99.99");
+        assertLoginAllowed(false, "User should be blocked after exceeding user-level attempts");
+
+        // Successful login from yet another client
+        TestRequestMetadataProvider.setBrowserCookie("successClient");
+        TestRequestMetadataProvider.setIpAddress("10.88.88.88");
+        loginAttemptsService.loginSucceeded(TestRequestMetadataProvider.getClientInfo());
+
+        // User-level lock must be cleared; a fresh attempt should now be allowed
+        TestRequestMetadataProvider.setBrowserCookie("brandNewClient");
+        TestRequestMetadataProvider.setIpAddress("10.77.77.77");
+        assertLoginAllowed(true, "User-level lock should be cleared after successful login from any client");
+    }
+
+    private String randomIp() {
+        return RandomStringUtils.randomNumeric(3) + "." +
+                RandomStringUtils.randomNumeric(3) + "." +
+                RandomStringUtils.randomNumeric(3) + "." +
+                RandomStringUtils.randomNumeric(3);
+    }
+
     private boolean simulateAndCheckLoginWithRandomizedClient(LoginAttemptsService serviceToTest) {
-        TestRequestMetadataProvider.setBrowserCookie(RandomStringUtils.randomAlphabetic(10)); // Unique client
+        TestRequestMetadataProvider.setBrowserCookie(RandomStringUtils.randomAlphabetic(10));
         serviceToTest.loginFailed(TestRequestMetadataProvider.getClientInfo());
         return serviceToTest.isAllowed(TestRequestMetadataProvider.getClientInfo());
     }
 
     private boolean simulateAndCheckLoginWithRandomizedClientAndIp(LoginAttemptsService serviceToTest) {
-        TestRequestMetadataProvider.setBrowserCookie(RandomStringUtils.randomAlphabetic(10)); // Unique client
-        TestRequestMetadataProvider.setIpAddress(RandomStringUtils.randomNumeric(3) + "." + RandomStringUtils.randomNumeric(3) + "." + RandomStringUtils.randomNumeric(3) + "." + RandomStringUtils.randomNumeric(3)); // Unique IP
+        TestRequestMetadataProvider.setBrowserCookie(RandomStringUtils.randomAlphabetic(10));
+        TestRequestMetadataProvider.setIpAddress(randomIp());
         serviceToTest.loginFailed(TestRequestMetadataProvider.getClientInfo());
         return serviceToTest.isAllowed(TestRequestMetadataProvider.getClientInfo());
     }
 
     @Test
     void givenSameIpUser_whenClientChangesAndMaxIpAttemptsExceeded_thenBlocked() {
-        // This test uses the default loginAttemptsService from setUp
         TestRequestMetadataProvider.setUserName("ipUserTest@example.com");
         TestRequestMetadataProvider.setIpAddress("10.0.0.1");
 
@@ -137,7 +169,6 @@ class LoginAttemptsServiceTest {
 
     @Test
     void givenSameUser_whenClientAndIpChangeAndMaxUserAttemptsExceeded_thenBlocked() {
-        // This test uses the default loginAttemptsService from setUp
         TestRequestMetadataProvider.setUserName("userOnlyTest@example.com");
 
         for (int i = 0; i < MAX_FAILED_USER_ATTEMPTS - 1; i++) {
@@ -150,8 +181,6 @@ class LoginAttemptsServiceTest {
                 .isFalse();
     }
 
-    // Removed static initRequestMetadata as it's handled by @BeforeEach/@AfterEach
-
     @Test
     void givenClientManuallyBlacklisted_whenLoginAttempted_thenBlocked() {
         loginAttemptsService.blacklistClient(TestRequestMetadataProvider.getClientInfo());
@@ -161,7 +190,6 @@ class LoginAttemptsServiceTest {
     @Test
     void givenAttemptsMade_whenCacheExpires_thenAttemptCountResets() {
         FakeTicker fakeTicker = new FakeTicker();
-        // Need a specific LoginAttemptsService instance that uses this fakeTicker
         LoginAttemptsService expiringService = new LoginAttemptsService(defaultDecisionVoter, fakeTicker);
 
         TestRequestMetadataProvider.setUserName("expiryTestUser@example.com");
@@ -176,7 +204,6 @@ class LoginAttemptsServiceTest {
         simulateFailedLogins(expiringService, 1);
         assertLoginAllowed(expiringService, true, "Allowed after cache expiry and 1 new attempt");
 
-        // Fill up to max attempts again
         simulateFailedLogins(expiringService, MAX_FAILED_CLIENT_ATTEMPTS - 2); // Already made 1, so N-2 more
         assertLoginAllowed(expiringService, true, "Still allowed just before hitting max attempts post-expiry");
 
@@ -187,7 +214,7 @@ class LoginAttemptsServiceTest {
     @Test
     void givenBlankUsername_whenLoginFailedMultipleTimes_thenNoAttemptsRecordedAndLoginAllowed() {
         TestRequestMetadataProvider.setUserName("");
-        simulateFailedLogins(MAX_FAILED_USER_ATTEMPTS + 5); // More than any threshold
+        simulateFailedLogins(MAX_FAILED_USER_ATTEMPTS + 5);
         assertLoginAllowed(true, "Login allowed with blank username despite many failures");
     }
 
@@ -207,9 +234,7 @@ class LoginAttemptsServiceTest {
     void givenBlankIpAddress_whenMaxAttemptsForIpUserExceeded_thenBlocked() {
         TestRequestMetadataProvider.setIpAddress("");
         TestRequestMetadataProvider.setUserName("userForBlankIpTest@example.com");
-        // Ensure client ID is non-blank for this test to focus on IP behavior
         TestRequestMetadataProvider.setBrowserCookie("clientForBlankIpScenario");
-
 
         simulateFailedLogins(MAX_FAILED_IP_ATTEMPTS - 1);
         assertLoginAllowed(true, "Allowed before max IP attempts with blank IP address");
@@ -218,5 +243,22 @@ class LoginAttemptsServiceTest {
         assertLoginAllowed(false, "Blocked after max IP attempts with blank IP address");
     }
 
+    /**
+     * Verifies that key separator characters inside field values do not cause cache key collisions
+     * between different (clientId, username) combinations.
+     */
+    @Test
+    void givenPipeCharacterInClientIdOrUsername_whenFailedLogins_thenNoKeyCollisionBetweenUsers() {
+        // user A: clientId = "abc|", username = "def"  → key must differ from
+        // user B: clientId = "abc",  username = "|def"
+        TestRequestMetadataProvider.setUserName("def");
+        TestRequestMetadataProvider.setBrowserCookie("abc|");
+        simulateFailedLogins(MAX_FAILED_CLIENT_ATTEMPTS);
+
+        // user B should not be affected by user A's failures
+        TestRequestMetadataProvider.setUserName("|def");
+        TestRequestMetadataProvider.setBrowserCookie("abc");
+        assertLoginAllowed(true, "User B must not be blocked by User A's failures due to key collision");
+    }
 
 }
